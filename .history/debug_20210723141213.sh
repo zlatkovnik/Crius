d60c58a041a70e0bd2f9855a34932ebc1f71b4af
zlatkovnik <zlatkovnik@dualsoft.net>
2021-07-23 15:08:00 +0200

    Izmena strukture

--- .history/debug_20210723141213.sh
+++ .history/debug_20210723141213.sh
@@ -1,5 +1,4 @@
 echo 'Compilation for' $0 'started'
-sudo g++ ./src/main.cpp glad.c -ldl -lglfw
+sudo g++ main.cpp glad.c -ldl -lglfw
 echo $0 'compiled successfully'
 ./a.out
-rm ./a.out
--- .history/debug_20210723150730.sh
+++ .history/debug_20210723150730.sh
@@ -2,4 +2,3 @@ echo 'Compilation for' $0 'started'
 sudo g++ ./src/main.cpp glad.c -ldl -lglfw
 echo $0 'compiled successfully'
 ./a.out
-rm ./a.out
